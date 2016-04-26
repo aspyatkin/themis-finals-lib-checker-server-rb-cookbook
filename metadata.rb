e31a7eabbad104167db0e78114c1889fece82356
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-26 20:16:37 +0400

    Draft

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'themis-finals-lib-checker-server-rb'
+description 'Installs and configures checker-server-rb'
+version '1.0.0'
+
+recipe 'themis-finals-lib-checker-server-rb', 'Installs and configures checker-server-rb'
+depends 'latest-git', '~> 1.1.6'
+depends 'git2', '~> 1.0.0'

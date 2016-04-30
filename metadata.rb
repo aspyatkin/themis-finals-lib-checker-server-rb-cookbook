424e0d43b258383be7c5a6c87867eda914518478
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-30 15:55:15 +0400

    Checkout develop by default

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-checker-server-rb'
 description 'Installs and configures checker-server-rb'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'themis-finals-lib-checker-server-rb', 'Installs and configures checker-server-rb'
 depends 'latest-git', '~> 1.1.6'

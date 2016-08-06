9cf1662724b2964b474d8dd6b149e22a1121538a
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-06 14:08:48 +0400

    Replace symbolized keys

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 id = 'themis-finals-lib-checker-server-rb'
 
-default[id][:basedir] = '/var/themis/finals/lib/checker-server-rb'
-default[id][:user] = 'vagrant'
-default[id][:group] = 'vagrant'
-default[id][:github_repository] = 'aspyatkin/themis-checker-server'
-default[id][:revision] = 'master'
+default[id]['basedir'] = '/var/themis/finals/lib/checker-server-rb'
+default[id]['user'] = 'vagrant'
+default[id]['group'] = 'vagrant'
+default[id]['github_repository'] = 'aspyatkin/themis-checker-server'
+default[id]['revision'] = 'master'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,15 +2,15 @@ include_recipe 'latest-git::default'
 
 id = 'themis-finals-lib-checker-server-rb'
 
-directory node[id][:basedir] do
-  owner node[id][:user]
-  group node[id][:group]
+directory node[id]['basedir'] do
+  owner node[id]['user']
+  group node[id]['group']
   mode 0755
   recursive true
   action :create
 end
 
-url_repository = "https://github.com/#{node[id][:github_repository]}"
+url_repository = "https://github.com/#{node[id]['github_repository']}"
 
 if node.chef_environment.start_with? 'development'
   ssh_data_bag_item = nil
@@ -22,16 +22,16 @@ if node.chef_environment.start_with? 'development'
   ssh_key_map = (ssh_data_bag_item.nil?) ? {} : ssh_data_bag_item.to_hash.fetch('keys', {})
 
   if ssh_key_map.size > 0
-    url_repository = "git@github.com:#{node[id][:github_repository]}.git"
+    url_repository = "git@github.com:#{node[id]['github_repository']}.git"
     ssh_known_hosts_entry 'github.com'
   end
 end
 
-git2 node[id][:basedir] do
+git2 node[id]['basedir'] do
   url url_repository
-  branch node[id][:revision]
-  user node[id][:user]
-  group node[id][:group]
+  branch node[id]['revision']
+  user node[id]['user']
+  group node[id]['group']
   action :create
 end
 
@@ -45,12 +45,12 @@ if node.chef_environment.start_with? 'development'
   git_options = (git_data_bag_item.nil?) ? {} : git_data_bag_item.to_hash.fetch('config', {})
 
   git_options.each do |key, value|
-    git_config "git-config #{key} at #{node[id][:basedir]}" do
+    git_config "git-config #{key} at #{node[id]['basedir']}" do
       key key
       value value
       scope 'local'
-      path node[id][:basedir]
-      user node[id][:user]
+      path node[id]['basedir']
+      user node[id]['user']
       action :set
     end
   end
